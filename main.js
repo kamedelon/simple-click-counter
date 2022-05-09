73b1a0da4a42ae0f145aa2bec8553174030410e7
KaMeDeLoN <98056932+kamedelon@users.noreply.github.com>
2022-05-09 22:20:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let click = document.querySelector('.click')
 let reset = document.querySelector('.reset')
+let popup = document.querySelector('.popup')
 let p = document.querySelector('p')
 let num = 0
 
 const clickCounter = () => { 
     num += 1
     p.innerHTML = num 
+
+    if(p.innerHTML == 100) {
+        popup.style.opacity = '1'
+    } else {
+        popup.style.opacity = '0'
+    }
 }
 
 const resetClicks = () => {
